e7ebac9e67ea1b0746446aa09aa0d1170d903bf0
Benjamin <kleynhansbenjamin@gmail.com>
2017-11-09 17:48:06 -0500

    Commented out connectivity configuration. Added JavaFX configuration

--- src/clientserver/Client.java
+++ src/clientserver/Client.java
@@ -1,89 +1,195 @@
 package clientserver;
 
-import java.io.*;
-import java.net.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
-public class Client extends JFrame {
-  // Text field for receiving radius
-  private JTextField jtf = new JTextField();
-  // Name of server to connect to
-  private final String SERVERNAME = "localhost";
-  // Port used for socket connections
-  private final int PORT = 8000;
-
-  // Text area to display contents
-  private JTextArea jta = new JTextArea();
-
-  // IO streams
-  private DataOutputStream toServer;
-  private DataInputStream fromServer;
-
-  public static void main(String[] args) {
-    new Client();
-  }
-
-  public Client() {
-    // Panel p to hold the label and text field
-    JPanel p = new JPanel();
-    p.setLayout(new BorderLayout());
-    p.add(new JLabel("Enter radius"), BorderLayout.WEST);
-    p.add(jtf, BorderLayout.CENTER);
-    jtf.setHorizontalAlignment(JTextField.RIGHT);
-
-    setLayout(new BorderLayout());
-    add(p, BorderLayout.NORTH);
-    add(new JScrollPane(jta), BorderLayout.CENTER);
-
-    jtf.addActionListener(new ButtonListener()); // Register listener
-
-    setTitle("Client");
-    setSize(500, 300);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true); // It is necessary to show the frame here!
-
-    try {
-      // Create a socket to connect to the server
-      Socket socket = new Socket(SERVERNAME, PORT);
-      // Socket socket = new Socket("130.254.204.36", 8000);
-      // Socket socket = new Socket("drake.Armstrong.edu", 8000);
-
-      // Create an input stream to receive data from the server
-      fromServer = new DataInputStream(
-        socket.getInputStream());
-
-      // Create an output stream to send data to the server
-      toServer =
-        new DataOutputStream(socket.getOutputStream());
+public class Client extends Application {
+    // Text area for displaying contents
+    private TextArea textArea = new TextArea();
+    // Port used for socket connections
+    private final int PORT = 8000;
+    private final Insets DEFAULT_INSETS = new Insets(15, 12, 15, 12);    
+    private final Insets DEFAULT_TOP_INSETS = new Insets(15, 0, 0, 0);
+    private final Insets DEFAULT_SIDE_INSETS = new Insets(0, 12, 0, 12);
+    private final Insets DEFAULT_BOTTOM_INSETS = new Insets(0, 0, 15, 0);
+    private final Insets DEFAULT_TOP_AND_BOTTOM_INSETS = new Insets(15, 0, 15, 0);
+  
+    @Override                                                                   // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        
+        BorderPane borderPane = new BorderPane();
+        
+        GridPane gridPane = new GridPane();
+        
+        HBox hBox = new HBox();
+        VBox vBox = new VBox();
+        
+        Button submitBtn = new Button("Submit");
+        
+        Label interestLbl = new Label("Annual Interest Rate");
+        Label periodLbl = new Label("Number Of Years");
+        Label amountLbl = new Label("Loan Amount");
+        
+        TextField interestTxtF = new TextField();        
+        TextField periodTxtF = new TextField();        
+        TextField amountTxtF = new TextField();
+        
+        GridPane.setHalignment(interestLbl, HPos.LEFT);
+        gridPane.setPadding(DEFAULT_INSETS);        
+        
+        vBox.setMinHeight(Region.USE_PREF_SIZE);
+        vBox.setPadding(DEFAULT_INSETS);                            // Define space formatting for vertical box
+        vBox.setAlignment(Pos.CENTER);
+        
+        hBox.setMinHeight(Region.USE_PREF_SIZE);
+        hBox.setPadding(DEFAULT_INSETS);
+        hBox.setAlignment(Pos.TOP_LEFT);        
+        
+        submitBtn.setMaxWidth(Double.MAX_VALUE);
+        submitBtn.setMinWidth(vBox.getPrefWidth());
+        submitBtn.setPadding(DEFAULT_INSETS);                            // Define space formatting for vertical box        
+        
+        interestLbl.setMinWidth(Region.USE_PREF_SIZE);
+        interestLbl.setPadding(DEFAULT_SIDE_INSETS);
+        
+        interestTxtF.setMinWidth(Region.USE_PREF_SIZE);
+        
+        periodLbl.setMinWidth(Region.USE_PREF_SIZE);
+        periodLbl.setPadding(DEFAULT_SIDE_INSETS);
+        
+        periodTxtF.setMinWidth(Region.USE_PREF_SIZE);
+        
+        amountLbl.setMinWidth(Region.USE_PREF_SIZE);
+        amountLbl.setPadding(DEFAULT_SIDE_INSETS);
+        
+        amountTxtF.setMinWidth(Region.USE_PREF_SIZE);
+        
+        GridPane.setConstraints(interestLbl, 0, 0);
+        GridPane.setConstraints(interestTxtF, 1, 0);
+        GridPane.setConstraints(periodLbl, 0, 1);
+        GridPane.setConstraints(periodTxtF, 1, 1);
+        GridPane.setConstraints(amountLbl, 0, 2);
+        GridPane.setConstraints(amountTxtF, 1, 2);
+        
+        gridPane.getChildren().addAll(
+                interestLbl,
+                interestTxtF,
+                periodLbl,
+                periodTxtF,
+                amountLbl,
+                amountTxtF
+            );
+        
+        vBox.getChildren().add(submitBtn);
+        
+        hBox.getChildren().add(textArea);
+        
+        borderPane.setCenter(gridPane);
+        borderPane.setRight(vBox);
+        borderPane.setBottom(hBox);
+        
+        Scene scene = new Scene(borderPane, 500, 350);                          // Create a scene containing a border pane with specified dimensions
+        
+        primaryStage.setTitle("Client");                                // Set the stage title
+        primaryStage.setScene(scene);                                           // Place the scene in the stage
+        primaryStage.show();                                                    // Display the stage
     }
-    catch (IOException ex) {
-      jta.append(ex.toString() + '\n');
-    }
-  }
 
-  private class ButtonListener implements ActionListener {
-    public void actionPerformed(ActionEvent e) {
-      try {
-        // Get the radius from the text field
-        double radius = Double.parseDouble(jtf.getText().trim());
-
-        // Send the radius to the server
-        toServer.writeDouble(radius);
-        toServer.flush();
-
-        // Get area from the server
-        double area = fromServer.readDouble();
-
-        // Display to the text area
-        jta.append("Radius is " + radius + "\n");
-        jta.append("Area received from the server is "
-          + area + '\n');
-      }
-      catch (IOException ex) {
-        System.err.println(ex);
-      }
-    }
-  }
+//// Text field for receiving radius
+//  private JTextField jtf = new JTextField();
+//  // Name of server to connect to
+//  private final String SERVERNAME = "localhost";
+//  // Port used for socket connections
+//  private final int PORT = 8000;
+//
+//  // Text area to display contents
+//  private JTextArea jta = new JTextArea();
+//
+//  // IO streams
+//  private DataOutputStream toServer;
+//  private DataInputStream fromServer;
+//
+//  public static void main(String[] args) {
+//    new Client();
+//  }
+//
+//  public Client() {
+//    // Panel p to hold the label and text field
+//    JPanel p = new JPanel();
+//    p.setLayout(new BorderLayout());
+//    p.add(new JLabel("Enter radius"), BorderLayout.WEST);
+//    p.add(jtf, BorderLayout.CENTER);
+//    jtf.setHorizontalAlignment(JTextField.RIGHT);
+//
+//    setLayout(new BorderLayout());
+//    add(p, BorderLayout.NORTH);
+//    add(new JScrollPane(jta), BorderLayout.CENTER);
+//
+//    jtf.addActionListener(new ButtonListener()); // Register listener
+//
+//    setTitle("Client");
+//    setSize(500, 300);
+//    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    setVisible(true); // It is necessary to show the frame here!
+//
+//    try {
+//      // Create a socket to connect to the server
+//      Socket socket = new Socket(SERVERNAME, PORT);
+//      // Socket socket = new Socket("130.254.204.36", 8000);
+//      // Socket socket = new Socket("drake.Armstrong.edu", 8000);
+//
+//      // Create an input stream to receive data from the server
+//      fromServer = new DataInputStream(
+//        socket.getInputStream());
+//
+//      // Create an output stream to send data to the server
+//      toServer =
+//        new DataOutputStream(socket.getOutputStream());
+//    }
+//    catch (IOException ex) {
+//      jta.append(ex.toString() + '\n');
+//    }
+//  }
+//
+//  private class ButtonListener implements ActionListener {
+//    public void actionPerformed(ActionEvent e) {
+//      try {
+//        // Get the radius from the text field
+//        double radius = Double.parseDouble(jtf.getText().trim());
+//
+//        // Send the radius to the server
+//        toServer.writeDouble(radius);
+//        toServer.flush();
+//
+//        // Get area from the server
+//        double area = fromServer.readDouble();
+//
+//        // Display to the text area
+//        jta.append("Radius is " + radius + "\n");
+//        jta.append("Area received from the server is "
+//          + area + '\n');
+//      }
+//      catch (IOException ex) {
+//        System.err.println(ex);
+//      }
+//    }
+//  }
 }
--- src/clientserver/Server.java
+++ src/clientserver/Server.java
@@ -1,61 +1,68 @@
 package clientserver;
 
-import java.io.*;
-import java.net.*;
-import java.util.*;
-import java.awt.*;
-import javax.swing.*;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 
-public class Server extends JFrame {
-  // Text area for displaying contents
-  private JTextArea jta = new JTextArea();
-  // Port used for socket connections
-  private final int PORT = 8000;
+public class Server extends Application {
+    // Text area for displaying contents
+    private TextArea textArea = new TextArea();
+    // Port used for socket connections
+    private final int PORT = 8000;
 
-  public static void main(String[] args) {
-    new Server();
-  }
+    @Override                                                                   // Override the start method in the Application class
+    public void start(Stage primaryStage) {
+        
+        BorderPane borderPane = new BorderPane();
+        
+        borderPane.setCenter(textArea);
+        
+        Scene scene = new Scene(borderPane, 400, 250);                          // Create a scene containing a border pane with specified dimensions
+        
+        primaryStage.setTitle("Server");                                // Set the stage title
+        primaryStage.setScene(scene);                                           // Place the scene in the stage
+        primaryStage.show();                                                    // Display the stage
 
-  public Server() {
-    // Place text area on the frame
-    setLayout(new BorderLayout());
-    add(new JScrollPane(jta), BorderLayout.CENTER);
-
-    setTitle("Server");
-    setSize(500, 300);
-    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    setVisible(true); // It is necessary to show the frame here!
-
-    try {
-      // Create a server socket
-      ServerSocket serverSocket = new ServerSocket(PORT);
-      jta.append("Server started at " + new Date() + '\n');
-
-      // Listen for a connection request
-      Socket socket = serverSocket.accept();
-
-      // Create data input and output streams
-      DataInputStream inputFromClient = new DataInputStream(
-        socket.getInputStream());
-      DataOutputStream outputToClient = new DataOutputStream(
-        socket.getOutputStream());
-
-      while (true) {
-        // Receive radius from the client
-        double radius = inputFromClient.readDouble();
-
-        // Compute area
-        double area = radius * radius * Math.PI;
-
-        // Send area back to the client
-        outputToClient.writeDouble(area);
-
-        jta.append("Radius received from client: " + radius + '\n');
-        jta.append("Area found: " + area + '\n');
-      }
+//        // Place text area on the frame
+//        setLayout(new BorderLayout());
+//        add(new JScrollPane(serverTextArea), BorderLayout.CENTER);
+//
+//        setTitle("Server");
+//        setSize(500, 300);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setVisible(true); // It is necessary to show the frame here!
+//
+//        try {
+//            // Create a server socket
+//            ServerSocket serverSocket = new ServerSocket(PORT);
+//            serverTextArea.append("Server started at " + new Date() + '\n');
+//
+//            // Listen for a connection request
+//            Socket socket = serverSocket.accept();
+//
+//            // Create data input and output streams
+//            DataInputStream inputFromClient = new DataInputStream(
+//                    socket.getInputStream());
+//            DataOutputStream outputToClient = new DataOutputStream(
+//                    socket.getOutputStream());
+//
+//            while (true) {
+//                // Receive radius from the client
+//                double radius = inputFromClient.readDouble();
+//
+//                // Compute area
+//                double area = radius * radius * Math.PI;
+//
+//                // Send area back to the client
+//                outputToClient.writeDouble(area);
+//
+//                serverTextArea.append("Radius received from client: " + radius + '\n');
+//                serverTextArea.append("Area found: " + area + '\n');
+//            }
+//        } catch (IOException ex) {
+//            System.err.println(ex);
+//        }
     }
-    catch(IOException ex) {
-      System.err.println(ex);
-    }
-  }
 }
