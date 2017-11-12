765e377f37546032f61037c78be04798cf185683
Benjamin <kleynhansbenjamin@gmail.com>
2017-11-11 19:16:38 -0500

    Coding completed Application working Commenting and documentation outstanding

--- src/clientserver/Client.java
+++ src/clientserver/Client.java
@@ -3,9 +3,11 @@ package clientserver;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,6 +18,7 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
@@ -24,85 +27,89 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.WindowEvent;
-import javafx.util.Duration;
 
 public class Client extends Application {
-    
-    // Name of server to connect to
-    private static final String SERVERNAME = "localhost";
-    // Port used for socket connections
-    private static final int PORT = 8000;
-    // Text area for displaying contents
-    private static TextArea outputTextArea = new TextArea();
-    // IO Streams
-    private static DataOutputStream toServer;
-    private static DataInputStream fromServer;
-    
+
+    // IO streams
+    private DataOutputStream primToServer = null;
+    private DataInputStream primFromServer = null;
+    private ObjectOutputStream objToServer = null;
+    private ObjectInputStream objFromServer = null;
+
+    private Socket socket = null;
+
+    private TextArea outputTextArea = new TextArea();
+
     private final Insets DEFAULT_INSETS = new Insets(7.5, 6, 7.5, 6);
-  
+
+    private TextField interestTxtF = new TextField();
+    private TextField periodTxtF = new TextField();
+    private TextField amountTxtF = new TextField();
+
     @Override                                                                   // Override the start method in the Application class
     public void start(Stage primaryStage) {
-        
+
+        primaryStage.setOnCloseRequest((WindowEvent we) -> {
+            System.out.println("Stage is closing");
+            System.exit(0);
+        });
+
         BorderPane borderPane = new BorderPane();
-        
+
         GridPane gridPane = new GridPane();
-        
+
         HBox hBox = new HBox();
         VBox vBox = new VBox();
-        
+
         hBox.setStyle("-fx-background-color: red");
         vBox.setStyle("-fx-background-color: blue");
         gridPane.setStyle("-fx-background-color: orange");
         borderPane.setStyle("-fx-background-color: purple");
-        
+
         Button submitBtn = new Button("Submit");
-        
+
         Label interestLbl = new Label("Annual Interest Rate");
         Label periodLbl = new Label("Number Of Years");
         Label amountLbl = new Label("Loan Amount");
-        
-        TextField interestTxtF = new TextField();        
-        TextField periodTxtF = new TextField();        
-        TextField amountTxtF = new TextField();
-        
+
         GridPane.setHalignment(interestLbl, HPos.LEFT);
-        gridPane.setPadding(DEFAULT_INSETS);        
-        
+        gridPane.setPadding(DEFAULT_INSETS);
+
         vBox.setMinHeight(Region.USE_PREF_SIZE);
         vBox.setMinWidth(Region.USE_PREF_SIZE);
         vBox.setPadding(DEFAULT_INSETS);                            // Define space formatting for vertical box
         vBox.setAlignment(Pos.CENTER);
-        
+
         hBox.setMinHeight(Region.USE_PREF_SIZE);
         hBox.setPadding(DEFAULT_INSETS);
-        hBox.setAlignment(Pos.TOP_LEFT);        
-        
+        hBox.setAlignment(Pos.TOP_LEFT);
+
         submitBtn.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
         submitBtn.setMinSize(Double.MIN_VALUE, Double.MIN_VALUE);
-        
+
         interestLbl.setMinWidth(Region.USE_PREF_SIZE);
         interestLbl.setPadding(DEFAULT_INSETS);
-        
+
         periodLbl.setMinWidth(Region.USE_PREF_SIZE);
         periodLbl.setPadding(DEFAULT_INSETS);
-        
+
         amountLbl.setMinWidth(Region.USE_PREF_SIZE);
         amountLbl.setPadding(DEFAULT_INSETS);
-        
+
         interestTxtF.setMinWidth(Region.USE_PREF_SIZE);
         interestTxtF.setAlignment(Pos.CENTER_RIGHT);
         periodTxtF.setMinWidth(Region.USE_PREF_SIZE);
         periodTxtF.setAlignment(Pos.CENTER_RIGHT);
         amountTxtF.setMinWidth(Region.USE_PREF_SIZE);
         amountTxtF.setAlignment(Pos.CENTER_RIGHT);
-        
+
         GridPane.setConstraints(interestLbl, 0, 0);
         GridPane.setConstraints(interestTxtF, 1, 0);
         GridPane.setConstraints(periodLbl, 0, 1);
         GridPane.setConstraints(periodTxtF, 1, 1);
         GridPane.setConstraints(amountLbl, 0, 2);
         GridPane.setConstraints(amountTxtF, 1, 2);
-        
+
         gridPane.getChildren().addAll(
                 interestLbl,
                 interestTxtF,
@@ -110,134 +117,107 @@ public class Client extends Application {
                 periodTxtF,
                 amountLbl,
                 amountTxtF
-            );
-        
+        );
+
         vBox.getChildren().add(submitBtn);
-        
-        //outputTextArea
-        //        .setAlignment(Pos.CENTER_RIGHT);
-        hBox.getChildren().add(outputTextArea);
-        
+
+        hBox.getChildren().add(new ScrollPane(outputTextArea));
+
         borderPane.setCenter(gridPane);
         borderPane.setRight(vBox);
         borderPane.setBottom(hBox);
-        
+
+//        new Thread(() -> {
+//            submitBtn.setOnAction(new ButtonListener());
+//        });
+        outputTextArea.appendText("Project 7 - Client/Server Technology\n");
+
+        try {
+            socket = new Socket("localhost", 8000);
+        } catch (IOException ex) {
+            outputTextArea.appendText(ex.toString() + '\n');
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        submitBtn.setOnAction(e -> submitData(socket));
+
         Scene scene = new Scene(borderPane, 450, 400);                          // Create a scene containing a border pane with specified dimensions
-        
-        primaryStage.setOnCloseRequest((WindowEvent t) -> {
-            System.out.println("CLOSING");
-        });
-        
+
         primaryStage.setTitle("Client");                                // Set the stage title
         primaryStage.setScene(scene);                                           // Place the scene in the stage
         primaryStage.show();                                                    // Display the stage
+
     }
-    
-    public static void connectToServer() {
-    
+//
+//    private class ButtonListener implements EventHandler<ActionEvent> {
+
+    private void submitData(Socket socket) {
+//
+//        @Override
+//        public void handle(ActionEvent e) {
+//            new Thread(() -> {
+
         try {
-            // Create a socket to connect to the server
-            Socket socket = new Socket(SERVERNAME, PORT);
-            // Socket socket = new Socket("130.254.204.36", 8000);
-            // Socket socket = new Socket("drake.Armstrong.edu", 8000);
+//                    Socket socket = new Socket("localhost", 8000);
 
-            // Create an input stream to receive data from the server
-            fromServer = new DataInputStream(
-              socket.getInputStream());
+            // Create primitive data type input and output streams
+            primFromServer = new DataInputStream(socket.getInputStream());
+            primToServer = new DataOutputStream(socket.getOutputStream());
 
-            // Create an output stream to send data to the server
-            toServer =
-              new DataOutputStream(socket.getOutputStream());
-          }
-          catch (IOException ex) {
+            // Create object type input and output streams
+            objToServer = new ObjectOutputStream(socket.getOutputStream());
+
+
+            String interestRate = interestTxtF.getText().trim();
+            String period = periodTxtF.getText().trim();
+            String amount = amountTxtF.getText().trim();
+
+            String toServerObj = interestRate + ":" + period + ":" + amount;
+
+
+            if (!interestRate.isEmpty() && !period.isEmpty() && !amount.isEmpty()) {
+                try {
+                    objToServer.writeObject(toServerObj);
+                    objToServer.flush();
+
+                    objFromServer = new ObjectInputStream(socket.getInputStream());
+
+                    Object object = null;
+
+                    try {
+                        object = objFromServer.readObject();
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    String[] fromServerObj = object.toString().split(":");
+
+                    outputTextArea.appendText(String.format("\nAnnual Interest Rate: %.2f\n", Double.parseDouble(interestRate)));
+                    outputTextArea.appendText(String.format("Number of Years: %d\n", Integer.parseInt(period)));
+                    outputTextArea.appendText(String.format("Loan Amount: %.2f\n", Double.parseDouble(amount)));
+                    outputTextArea.appendText(String.format("monthlyPayment: %.2f\n", Double.parseDouble(fromServerObj[0]))); 	// Get the monthly payment from the object, format it to look good and display it in the textbox
+                    outputTextArea.appendText(String.format("totalPayment: %.2f\n", Double.parseDouble(fromServerObj[1])));
+
+                } catch (IOException ex) {
+                    outputTextArea.appendText(ex.toString() + '\n');
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            } else {
+            
+                outputTextArea.appendText("\n!!! Please enter all the required values before pressing submit !!!\n");
+            
+            }
+        } catch (IOException ex) {
             outputTextArea.appendText(ex.toString() + '\n');
-          }
-    }
-    
-    public static void main (String[] args) {
-    
-        connectToServer();
-        launch(args);
-    
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
     }
 
-//// Text field for receiving radius
-//  private JTextField jtf = new JTextField();
-//  // Name of server to connect to
-//  private final String SERVERNAME = "localhost";
-//  // Port used for socket connections
-//  private final int PORT = 8000;
-//
-//  // Text area to display contents
-//  private JTextArea jta = new JTextArea();
-//
-//  // IO streams
-//  private DataOutputStream toServer;
-//  private DataInputStream fromServer;
-//
-//  public static void main(String[] args) {
-//    new Client();
-//  }
-//
-//  public Client() {
-//    // Panel p to hold the label and text field
-//    JPanel p = new JPanel();
-//    p.setLayout(new BorderLayout());
-//    p.add(new JLabel("Enter radius"), BorderLayout.WEST);
-//    p.add(jtf, BorderLayout.CENTER);
-//    jtf.setHorizontalAlignment(JTextField.RIGHT);
-//
-//    setLayout(new BorderLayout());
-//    add(p, BorderLayout.NORTH);
-//    add(new JScrollPane(jta), BorderLayout.CENTER);
-//
-//    jtf.addActionListener(new ButtonListener()); // Register listener
-//
-//    setTitle("Client");
-//    setSize(500, 300);
-//    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//    setVisible(true); // It is necessary to show the frame here!
-//
-//    try {
-//      // Create a socket to connect to the server
-//      Socket socket = new Socket(SERVERNAME, PORT);
-//      // Socket socket = new Socket("130.254.204.36", 8000);
-//      // Socket socket = new Socket("drake.Armstrong.edu", 8000);
-//
-//      // Create an input stream to receive data from the server
-//      fromServer = new DataInputStream(
-//        socket.getInputStream());
-//
-//      // Create an output stream to send data to the server
-//      toServer =
-//        new DataOutputStream(socket.getOutputStream());
-//    }
-//    catch (IOException ex) {
-//      jta.append(ex.toString() + '\n');
-//    }
-//  }
-//
-//  private class ButtonListener implements ActionListener {
-//    public void actionPerformed(ActionEvent e) {
-//      try {
-//        // Get the radius from the text field
-//        double radius = Double.parseDouble(jtf.getText().trim());
-//
-//        // Send the radius to the server
-//        toServer.writeDouble(radius);
-//        toServer.flush();
-//
-//        // Get area from the server
-//        double area = fromServer.readDouble();
-//
-//        // Display to the text area
-//        jta.append("Radius is " + radius + "\n");
-//        jta.append("Area received from the server is "
-//          + area + '\n');
-//      }
-//      catch (IOException ex) {
-//        System.err.println(ex);
-//      }
-//    }
-//  }
+    public static void main(String[] args) throws IOException {
+
+        launch(args);
+
+    }
 }
--- src/clientserver/Server.java
+++ src/clientserver/Server.java
@@ -1,68 +1,162 @@
 package clientserver;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.WindowEvent;
 
 public class Server extends Application {
-    // Text area for displaying contents
-    private TextArea textArea = new TextArea();
-    // Port used for socket connections
-    private final int PORT = 8000;
+    
+    private TextArea outputTextArea = new TextArea();
+    // Number a client
+    private int clientNo = 0;
 
     @Override                                                                   // Override the start method in the Application class
     public void start(Stage primaryStage) {
         
-        BorderPane borderPane = new BorderPane();
-        
-        borderPane.setCenter(textArea);
-        
-        Scene scene = new Scene(borderPane, 400, 250);                          // Create a scene containing a border pane with specified dimensions
-        
-        primaryStage.setTitle("Server");                                // Set the stage title
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                System.out.println("Stage is closing");
+                System.exit(0);
+            }
+        });
+
+        Scene scene = new Scene(new ScrollPane(outputTextArea), 500, 200);                          // Create a scene containing a border pane with specified dimensions
+
+        primaryStage.setTitle("MultiThreadedServer");                                // Set the stage title
         primaryStage.setScene(scene);                                           // Place the scene in the stage
         primaryStage.show();                                                    // Display the stage
+        
+        outputTextArea.appendText("Project 7 - Client/Server Technology\n");
 
-//        // Place text area on the frame
-//        setLayout(new BorderLayout());
-//        add(new JScrollPane(serverTextArea), BorderLayout.CENTER);
-//
-//        setTitle("Server");
-//        setSize(500, 300);
-//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        setVisible(true); // It is necessary to show the frame here!
-//
-//        try {
-//            // Create a server socket
-//            ServerSocket serverSocket = new ServerSocket(PORT);
-//            serverTextArea.append("Server started at " + new Date() + '\n');
-//
-//            // Listen for a connection request
-//            Socket socket = serverSocket.accept();
-//
-//            // Create data input and output streams
-//            DataInputStream inputFromClient = new DataInputStream(
-//                    socket.getInputStream());
-//            DataOutputStream outputToClient = new DataOutputStream(
-//                    socket.getOutputStream());
-//
-//            while (true) {
-//                // Receive radius from the client
-//                double radius = inputFromClient.readDouble();
-//
-//                // Compute area
-//                double area = radius * radius * Math.PI;
-//
-//                // Send area back to the client
-//                outputToClient.writeDouble(area);
-//
-//                serverTextArea.append("Radius received from client: " + radius + '\n');
-//                serverTextArea.append("Area found: " + area + '\n');
-//            }
-//        } catch (IOException ex) {
-//            System.err.println(ex);
-//        }
+        new Thread( () -> {
+            try {
+
+                // Create a socket to connect to the server
+                ServerSocket serverSocket = new ServerSocket(8000);
+
+                //ServerSocket serverSocket = new ServerSocket(PORT);
+                outputTextArea.appendText("MultiThreadServer started at " + new Date() + '\n');
+
+                while (true) {
+
+                    // Listen for a connection request
+                    Socket socket = serverSocket.accept();
+
+                    // Increment clientNo
+                    clientNo++;
+                    
+                    Platform.runLater( () -> {
+                        // Display the client number
+                        outputTextArea.appendText("\nStarting thread for client " + clientNo + " at " + new Date() + '\n');
+                        
+                        // Find the client's host name, and IP address
+                        InetAddress inetAddress = socket.getInetAddress();
+                        outputTextArea.appendText("Client " + clientNo + "'s host name is " + inetAddress.getHostName() + "\n");
+                        outputTextArea.appendText("Client " + clientNo + "'s IP Address is " + inetAddress.getHostAddress() + "\n");
+                    });
+                    
+                    // Create and start a new thread for the connection
+                    new Thread(new HandleAClient(socket)).start();
+
+                }
+            } catch (IOException ex) {
+                System.err.println(ex);
+            } 
+        }).start();           
     }
+
+    class HandleAClient implements Runnable {
+    
+        private Socket socket; // A connected socket
+        
+        /** Construct a thread */
+        public HandleAClient(Socket socket) {
+            this.socket = socket;        
+        }
+        
+        /** Run a thread */
+        @Override
+        public void run() {
+        
+            try {
+System.out.println("test001");            
+                // Create primitive data type input and output streams
+                DataInputStream primFromClient = new DataInputStream(socket.getInputStream());
+                DataOutputStream primToClient = new DataOutputStream(socket.getOutputStream());
+System.out.println("test002");                
+                // Create object type input and output streams                
+                ObjectInputStream objFromClient;
+                ObjectOutputStream objToClient;
+                
+System.out.println("test003");                
+                // Continuously serve the client
+                while (true) {
+                    
+                    objFromClient = new ObjectInputStream(socket.getInputStream());
+                    objToClient = new ObjectOutputStream(socket.getOutputStream());
+                
+                    Object object = null;
+
+                    try {
+                        object = objFromClient.readObject();
+                    } catch (ClassNotFoundException ex) {
+                        Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+
+                    String[] fromClientObj = object.toString().split(":");
+
+                    Loan loan = new Loan(
+                            Double.parseDouble(fromClientObj[0]), 
+                            Integer.parseInt(fromClientObj[1]), 
+                            Double.parseDouble(fromClientObj[2])
+                    );
+
+                    Platform.runLater(() -> {
+                        outputTextArea.appendText(String.format("\nAnnual Interest Rate: %.2f\n", loan.getAnnualInterestRate()));
+                        outputTextArea.appendText(String.format("Number of Years: %d\n", loan.getNumberOfYears()));
+                        outputTextArea.appendText(String.format("Loan Amount: %.2f\n", loan.getLoanAmount()));
+                        outputTextArea.appendText(String.format("monthlyPayment: %.2f\n", loan.getMonthlyPayment())); 	// Get the monthly payment from the object, format it to look good and display it in the textbox
+                        outputTextArea.appendText(String.format("totalPayment: %.2f\n", loan.getTotalPayment())); 
+                        
+                    });
+
+                    String toClientObj = loan.getMonthlyPayment() + ":" + loan.getTotalPayment();
+                    
+                    objToClient.writeObject(toClientObj);
+                    objToClient.flush();                    
+                
+                }
+                            
+            } catch (IOException ex) {
+                outputTextArea.appendText(ex.toString() + '\n');
+                    Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);            
+            }
+        
+        }
+    
+    }
+    
+    public static void main (String[] args) {
+    
+        launch(args);
+    
+    }
+    
 }
